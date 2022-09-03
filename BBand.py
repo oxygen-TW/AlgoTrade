f9e3778d85312026d5cb980d0f80946de37e907f
oxygen-TW <howard2805@gmail.com>
2022-09-03 16:38:37 +0800

    Add StdN argument.

--- BBand.py
+++ BBand.py
@@ -7,14 +7,15 @@ from DataHandler.commonTools import DataIO
 
 #reference: https://medium.com/codex/how-to-calculate-bollinger-bands-of-a-stock-with-python-f9f7d1184fc3
 class BollingerBands():
-    def __init__(self, stockData: pd.DataFrame, rate=20):
+    def __init__(self, stockData: pd.DataFrame, days=20, stdN=2):
         self.stockData = stockData
 
         assert(type(stockData) == pd.DataFrame), "stockData must be a DataFrame"
         assert("Close" in self.stockData.columns), "Close column not found in stockData"
 
         self.closeingPrice = stockData['Close'] 
-        self.rate = rate
+        self.rate = days
+        self.stdN = stdN
         self.bollinger_up, self.bollinger_down = self.calculateBollingerBands()
     
     def calculateSMA(self):
@@ -23,15 +24,16 @@ class BollingerBands():
     def calculateBollingerBands(self):
         sma = self.calculateSMA()
         std = self.closeingPrice.rolling(self.rate).std()
-        bollinger_up = sma + std * 2 # Calculate top band
-        bollinger_down = sma - std * 2 # Calculate bottom band
+        bollinger_up = sma + std * self.stdN # Calculate top band
+        bollinger_down = sma - std * self.stdN # Calculate bottom band
         return bollinger_up, bollinger_down
     
     def drawPicture(self):
         plt.figure(figsize=(12, 8))
+        plt.title(symbol + ' B-Bands')
         plt.plot(self.stockData['Close'], label='Close')
-        plt.plot(self.bollinger_up, label='Bollinger up')
-        plt.plot(self.bollinger_down, label='Bollinger down')
+        plt.plot(self.bollinger_up, label='Bollinger up; SD*'+str(self.stdN))
+        plt.plot(self.bollinger_down, label='Bollinger down; SD*'+str(self.stdN))
         plt.legend(loc='upper left')
         plt.show()
 
@@ -49,6 +51,6 @@ if(__name__ == "__main__"):
     df = DataIO.readCSV(os.path.join("stockData", symbol + ".csv"))
     df.index = np.arange(df.shape[0]) # Convert the index to array from [0, 1, 2, ...number of rows]
 
-    bb = BollingerBands(df)
-    print(bb.getBollingerBands_Up())
+    bb = BollingerBands(df, stdN=2.1)
+    bb.drawPicture()
 
